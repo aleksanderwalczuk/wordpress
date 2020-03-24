d7a0bca44bd3fb486349c2d88a502d893f8430f0
rednaxela700 <ohleg@windowslive.com>
2020-03-24 09:43:59 +0100

    archive.php: set markup and bs4 classes for post category view

--- wp-content/themes/newtheme/archive.php
+++ wp-content/themes/newtheme/archive.php
@@ -2,23 +2,27 @@
 //blog archive - available via WPadmin - posts - categories
 get_header();
 ?>
-	<div class="container pt-5 pb-5">
-		<div class="row">
-			<h1><?php single_cat_title() ?></h1>
-		</div>
-		<div class="row">
-			<div class="col">
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <h1><?php single_cat_title() ?></h1>
+        </div>
+        <div class="row">
+            <div class="col">
 				<?php
 				//            if there are posts while we have posts then show the post
 				if ( have_posts() ) : while ( have_posts() ) :the_post();
 					?>
-                    <h4><?php the_title(); ?></h4>
-					<?php the_excerpt(); ?>
-                    <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h4><?php the_title(); ?></h4>
+							<?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
 
 				<?php endwhile; endif; ?>
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 <?php get_footer(); ?>
