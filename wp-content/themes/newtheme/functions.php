14b51131821cfa1f762fa6e0c1fcb315a6a30bbc
rednaxela700 <ohleg@windowslive.com>
2020-03-24 07:13:28 +0100

    functions.php: added bootstrap to the project

---
+++ wp-content/themes/newtheme/functions.php
@@ -0,0 +1,8 @@
+<?php //add here all links to head
+function load_stylesheets() {
+	$url = ['bootstrap' => "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"];
+	wp_register_style('bootstrap', $url('bootstrap'), array());
+	wp_enqueue_style('bootstrap');
+}
+
+add_action('wp_enqueue_scripts', 'load_stylesheets');
