6979a0f752e60345bae22c49fc4b5d0a5048b8bd
rednaxela700 <ohleg@windowslive.com>
2020-03-24 07:18:39 +0100

    functions.php: fixed prev commit error

--- wp-content/themes/newtheme/functions.php
+++ wp-content/themes/newtheme/functions.php
@@ -1,7 +1,7 @@
 <?php //add here all links to head
 function load_stylesheets() {
 	$url = ['bootstrap' => "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"];
-	wp_register_style('bootstrap', $url('bootstrap'), array());
+	wp_register_style('bootstrap', $url['bootstrap'], array(), false, 'all');
 	wp_enqueue_style('bootstrap');
 }
 
