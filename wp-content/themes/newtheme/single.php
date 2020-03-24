400972a2e5400ca00a51fb20ba41b913858d6bfb
rednaxela700 <ohleg@windowslive.com>
2020-03-24 09:25:58 +0100

    single.php: copied content from page.php

--- wp-content/themes/newtheme/single.php
+++ wp-content/themes/newtheme/single.php
@@ -1,2 +1,23 @@
 <?php
 //single blog post
+get_header();
+?>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <h1><?php the_title() ?></h1>
+        </div>
+        <div class="row">
+            <div class="col">
+				<?php
+				//            if there are posts while we have posts then show the post
+				if ( have_posts() ) : while ( have_posts() ) :the_post();
+					?>
+
+					<?php the_content(); ?>
+
+				<?php endwhile; endif; ?>
+
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
