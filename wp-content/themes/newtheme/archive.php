1e38a259652d5f0c414eda83a4c7540f0a36ceb0
rednaxela700 <ohleg@windowslive.com>
2020-03-24 09:34:22 +0100

    basic setup for posts archive

--- wp-content/themes/newtheme/archive.php
+++ wp-content/themes/newtheme/archive.php
@@ -1,2 +1,24 @@
 <?php
-//blog archive
+//blog archive - available via WPadmin - posts - categories
+get_header();
+?>
+	<div class="container pt-5 pb-5">
+		<div class="row">
+			<h1><?php single_cat_title() ?></h1>
+		</div>
+		<div class="row">
+			<div class="col">
+				<?php
+				//            if there are posts while we have posts then show the post
+				if ( have_posts() ) : while ( have_posts() ) :the_post();
+					?>
+                    <h4><?php the_title(); ?></h4>
+					<?php the_excerpt(); ?>
+                    <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+
+				<?php endwhile; endif; ?>
+
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
