5376a3111c19dc0719a8128a66d1160d0064d898
rednaxela700 <ohleg@windowslive.com>
2020-03-24 07:28:42 +0100

    functions.php: added my css file

--- wp-content/themes/newtheme/functions.php
+++ wp-content/themes/newtheme/functions.php
@@ -1,8 +1,14 @@
 <?php //add here all links to head
 function load_stylesheets() {
-	$url = ['bootstrap' => "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"];
+	$url = [
+		'bootstrap' => "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+		'myCss' => '/style.css'
+	];
 	wp_register_style('bootstrap', $url['bootstrap']);
 	wp_enqueue_style('bootstrap');
+
+	wp_register_style('myStyles', get_template_directory_uri() . $url['myCss']);
+	wp_enqueue_style('myStyles');
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
